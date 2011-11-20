c4b3ad3aeb444633feb424c7b333e91c5bdd7e9f
Carlos Morillo Merino <carlos@affjet.com>
2011-11-20 16:19:11 +0000

    modified:   Oara/Network/CommissionJunction.php

--- Oara/Network/CommissionJunction.php
+++ Oara/Network/CommissionJunction.php
@@ -398,9 +398,8 @@ class Oara_Network_CommissionJunction extends Oara_Network{
 	 */
 	public function getPaymentHistory(){
     	$paymentHistory = array();
-    	
     	$urls = array();
-    	$urls[] = new Oara_Curl_Request('https://members.cj.com/member/'.$this->_memberId.'/publisher/displaypublisherpaymentstatus.do?ispopup=true', array());   
+    	$urls[] = new Oara_Curl_Request('https://members.cj.com/member/cj/publisher/paymentStatus', array());   
     	$exportReport = $this->_client->get($urls);
         if (preg_match("/\/publisher\/getpublisherpaymenthistory\.do/", $exportReport[0], $matches)){
 	    	$urls = array();
