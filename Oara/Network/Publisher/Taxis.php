b379e0e7056c2d723157028dcd5130604e0f256b
Carlos Morillo <carlos@affjet.com>
2012-12-05 11:07:21 +0000

    modified:   Oara/Network/Publisher/Taxis.php

--- Oara/Network/Publisher/Taxis.php
+++ Oara/Network/Publisher/Taxis.php
@@ -294,43 +294,44 @@ class Oara_Network_Publisher_Taxis extends Oara_Network {
 
 		foreach ($transactionArray as $merchantId => $merchantTransaction) {
 			foreach ($merchantTransaction as $date => $transactionList) {
-
-				$overview = Array();
-
-				$overview['merchantId'] = $merchantId;
-				$overviewDate = new Zend_Date($date, "yyyy-MM-dd");
-				$overview['date'] = $overviewDate->toString("yyyy-MM-dd HH:mm:ss");
-				$overview['click_number'] = 0;
-				$overview['impression_number'] = 0;
-				$overview['transaction_number'] = 0;
-				$overview['transaction_confirmed_value'] = 0;
-				$overview['transaction_confirmed_commission'] = 0;
-				$overview['transaction_pending_value'] = 0;
-				$overview['transaction_pending_commission'] = 0;
-				$overview['transaction_declined_value'] = 0;
-				$overview['transaction_declined_commission'] = 0;
-				$overview['transaction_paid_value'] = 0;
-				$overview['transaction_paid_commission'] = 0;
-				foreach ($transactionList as $transaction) {
-					$overview['transaction_number']++;
-					if ($transaction['status'] == Oara_Utilities::STATUS_CONFIRMED) {
-						$overview['transaction_confirmed_value'] += $transaction['amount'];
-						$overview['transaction_confirmed_commission'] += $transaction['commission'];
-					} else
-						if ($transaction['status'] == Oara_Utilities::STATUS_PENDING) {
-							$overview['transaction_pending_value'] += $transaction['amount'];
-							$overview['transaction_pending_commission'] += $transaction['commission'];
+				if (in_array($merchantId, $merchantList)) {
+					$overview = Array();
+	
+					$overview['merchantId'] = $merchantId;
+					$overviewDate = new Zend_Date($date, "yyyy-MM-dd");
+					$overview['date'] = $overviewDate->toString("yyyy-MM-dd HH:mm:ss");
+					$overview['click_number'] = 0;
+					$overview['impression_number'] = 0;
+					$overview['transaction_number'] = 0;
+					$overview['transaction_confirmed_value'] = 0;
+					$overview['transaction_confirmed_commission'] = 0;
+					$overview['transaction_pending_value'] = 0;
+					$overview['transaction_pending_commission'] = 0;
+					$overview['transaction_declined_value'] = 0;
+					$overview['transaction_declined_commission'] = 0;
+					$overview['transaction_paid_value'] = 0;
+					$overview['transaction_paid_commission'] = 0;
+					foreach ($transactionList as $transaction) {
+						$overview['transaction_number']++;
+						if ($transaction['status'] == Oara_Utilities::STATUS_CONFIRMED) {
+							$overview['transaction_confirmed_value'] += $transaction['amount'];
+							$overview['transaction_confirmed_commission'] += $transaction['commission'];
 						} else
-							if ($transaction['status'] == Oara_Utilities::STATUS_DECLINED) {
-								$overview['transaction_declined_value'] += $transaction['amount'];
-								$overview['transaction_declined_commission'] += $transaction['commission'];
+							if ($transaction['status'] == Oara_Utilities::STATUS_PENDING) {
+								$overview['transaction_pending_value'] += $transaction['amount'];
+								$overview['transaction_pending_commission'] += $transaction['commission'];
 							} else
-								if ($transaction['status'] == Oara_Utilities::STATUS_PAID) {
-									$overview['transaction_paid_value'] += $transaction['amount'];
-									$overview['transaction_paid_commission'] += $transaction['commission'];
-								}
+								if ($transaction['status'] == Oara_Utilities::STATUS_DECLINED) {
+									$overview['transaction_declined_value'] += $transaction['amount'];
+									$overview['transaction_declined_commission'] += $transaction['commission'];
+								} else
+									if ($transaction['status'] == Oara_Utilities::STATUS_PAID) {
+										$overview['transaction_paid_value'] += $transaction['amount'];
+										$overview['transaction_paid_commission'] += $transaction['commission'];
+									}
+					}
+					$overviewArray[] = $overview;
 				}
-				$overviewArray[] = $overview;
 			}
 		}
 
