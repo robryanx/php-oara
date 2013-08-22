b263eb432a6797c5905f8a55289510cec06f8e8e
Pavel Smolka <pavel.smolka@futurenet.com>
2013-08-22 11:03:58 +0100

    Composer handles ZF dependency and autoloading
    Composer support has been added. Using 'composer update' downloads
    the Zend Framework 1 dependency, from Packagist (can be changed in
    the 'composer.json' file). There is no need to use git submodules.
    
    Composer also handles autoloading the classes. Even though ZF1
    uses 'require_once' statements, using Composer to do all autoload
    is working.

--- settings.php
+++ settings.php
@@ -1,4 +1,8 @@
 <?php
+
+// Use composer autoload to load OARA classes as well as Zend Framework (dependency)
+require_once 'vendor/autoload.php';
+
 //Upgrade the memory limit
 ini_set('memory_limit', '256M');
 // Error handle configuration
@@ -11,15 +15,6 @@ define('DS', DIRECTORY_SEPARATOR);
 date_default_timezone_set('GMT');
 
 umask(0002);
-//Defining the paths
-$paths[] = BI_PATH_BASE.DS.'vendor/ZendFramework/library';
-$paths[] = BI_PATH_BASE;
-set_include_path(implode(PATH_SEPARATOR, $paths));
-
-// Setting up Zend Auto Loader
-require_once 'Zend/Loader/Autoloader.php';
-$autoloader = Zend_Loader_Autoloader::getInstance();
-$autoloader->setFallbackAutoloader(true);
 
 //Adding the credentials ini into the Zend Registry
 $config = new Zend_Config_Ini(BI_PATH_BASE.DS.'examples/credentials.ini', 'production');
