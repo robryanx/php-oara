4ba90164544edd39b4df8afecb4b6f1eae105a27
Carlos Morillo <carlos@affjet.com>
2014-02-13 17:47:54 +0000

    new file:   Oara/Network/Publisher/RentalCars.php 	deleted:    Oara/Network/Publisher/TravelJigsaw.php

---
+++ Oara/Network/Publisher/RentalCars.php
@@ -0,0 +1,274 @@
+<?php
+/**
+ * Export Class
+ *
+ * @author     Carlos Morillo Merino
+ * @category   Oara_Network_Publisher_RentalCars
+ * @copyright  Fubra Limited
+ * @version    Release: 01.00
+ *
+ */
+class Oara_Network_Publisher_RentalCars extends Oara_Network {
+	private $_credentials = null;
+	/**
+	 * Client
+	 * 
+	 * @var unknown_type
+	 */
+	private $_client = null;
+	/**
+	 * Constructor and Login
+	 * 
+	 * @param
+	 *        	$credentials
+	 * @return Oara_Network_Publisher_PureVPN
+	 */
+	public function __construct($credentials) {
+		$this->_credentials = $credentials;
+		self::logIn ();
+	}
+	private function logIn() {
+		$valuesLogin = array (
+				new Oara_Curl_Parameter ( 'login_username', $this->_credentials ['user'] ),
+				new Oara_Curl_Parameter ( 'login_password', $this->_credentials ['password'] ) 
+		);
+		
+		$loginUrl = 'https://secure.rentalcars.com/affiliates/access?commit=true';
+		$this->_client = new Oara_Curl_Access ( $loginUrl, $valuesLogin, $this->_credentials );
+		
+		if (! self::checkConnection ()) {
+			throw new Exception ( "You are not connected\n\n" );
+		}
+	}
+	/**
+	 * Check the connection
+	 */
+	public function checkConnection() {
+		// If not login properly the construct launch an exception
+		$connection = false;
+		$urls = array ();
+		$urls [] = new Oara_Curl_Request ( 'https://secure.rentalcars.com/affiliates/?master=1', array () );
+		
+		$exportReport = $this->_client->get ( $urls );
+		
+		$dom = new Zend_Dom_Query ( $exportReport [0] );
+		$results = $dom->query ( '#header_logout' );
+		
+		if (count ( $results ) > 0) {
+			$connection = true;
+		}
+		return $connection;
+	}
+	/**
+	 * (non-PHPdoc)
+	 * 
+	 * @see library/Oara/Network/Oara_Network_Publisher_Interface#getMerchantList()
+	 */
+	public function getMerchantList() {
+		$merchants = array ();
+		
+		$obj = array ();
+		$obj ['cid'] = "1";
+		$obj ['name'] = "RentalCars";
+		$obj ['url'] = "https://secure.rentalcars.com";
+		$merchants [] = $obj;
+		
+		return $merchants;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * 
+	 * @see library/Oara/Network/Oara_Network_Publisher_Interface#getTransactionList($aMerchantIds, $dStartDate, $dEndDate, $sTransactionStatus)
+	 */
+	public function getTransactionList($merchantList = null, Zend_Date $dStartDate = null, Zend_Date $dEndDate = null, $merchantMap = null) {
+		$totalTransactions = array ();
+		$valuesFormExport = array ();
+		
+		$valuesFormExport = array ();
+		$valuesFormExport [] = new Oara_Curl_Parameter ( 'booking', 'booking' );
+		$valuesFormExport [] = new Oara_Curl_Parameter ( 'cancelled', 'cancelled' );
+		
+		$urls = array ();
+		$urls [] = new Oara_Curl_Request ( 'https://secure.rentalcars.com/affiliates/booked_excel?date_start=' . $dStartDate->toString ( "yyyy-MM-dd" ) . '&date_end=' . $dEndDate->toString ( "yyyy-MM-dd" ) . '?', $valuesFormExport );
+		$exportReport = $this->_client->post ( $urls );
+		
+		$xml = simplexml_load_string ( $exportReport [0] );
+		$json = json_encode ( $xml );
+		$array = json_decode ( $json, TRUE );
+		
+		$headerIndex = array();
+		for ($i=0; $i < count($array["Worksheet"]["Table"]["Row"][2]["Cell"]);$i++){
+			$headerIndex[$i] = $array["Worksheet"]["Table"]["Row"][2]["Cell"][$i]["Data"];
+		}
+		
+		
+		for($z = 3; $z < count ( $array["Worksheet"]["Table"]["Row"] ) - 2; $z ++) {
+			$transactionDetails = array();
+			for ($i=0; $i < count($array["Worksheet"]["Table"]["Row"][$z]["Cell"]);$i++){
+				$transactionDetails[$headerIndex[$i]] = $array["Worksheet"]["Table"]["Row"][$z]["Cell"][$i]["Data"];
+			}
+			
+			$transaction = Array ();
+			$transaction ['merchantId'] = "1";
+			$transaction ['unique_id'] = $transactionDetails["Res. Number"];
+			$date = new Zend_Date($transactionDetails["Book Date"], "dd MMM yyyy - HH:ii", "en_GB");
+			
+			$transaction ['custom_id'] = $transactionDetails["AD Campaign"];
+			
+			$transaction ['date'] = $date->toString ( "yyyy-MM-dd HH:mm:00" );
+			$transaction ['status'] = Oara_Utilities::STATUS_CONFIRMED;
+			$transaction ['amount'] = $transactionDetails["Booking Value"];
+			$transaction ['currency'] = $transactionDetails["Payment Currency"];
+			$transaction ['commission'] = $transactionDetails["Total Commission"];
+			$totalTransactions [] = $transaction;
+			
+		}
+		return $totalTransactions;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * 
+	 * @see library/Oara/Network/Oara_Network_Publisher_Base#getOverviewList($merchantId, $dStartDate, $dEndDate)
+	 */
+	public function getOverviewList($transactionList = null, $merchantList = null, Zend_Date $dStartDate = null, Zend_Date $dEndDate = null, $merchantMap = null) {
+		$overviewArray = Array();
+		$transactionArray = Oara_Utilities::transactionMapPerDay($transactionList);
+
+
+		//Add transactions
+		foreach ($transactionArray as $merchantId => $merchantTransaction) {
+			foreach ($merchantTransaction as $date => $transactionList) {
+
+				$overview = Array();
+
+				$overview['merchantId'] = $merchantId;
+				$overviewDate = new Zend_Date($date, "yyyy-MM-dd");
+				$overview['date'] = $overviewDate->toString("yyyy-MM-dd HH:mm:ss");
+				unset($overviewDate);
+				$overview['click_number'] = 0;
+				$overview['impression_number'] = 0;
+				$overview['transaction_number'] = 0;
+				$overview['transaction_confirmed_value'] = 0;
+				$overview['transaction_confirmed_commission'] = 0;
+				$overview['transaction_pending_value'] = 0;
+				$overview['transaction_pending_commission'] = 0;
+				$overview['transaction_declined_value'] = 0;
+				$overview['transaction_declined_commission'] = 0;
+				$overview['transaction_paid_value'] = 0;
+				$overview['transaction_paid_commission'] = 0;
+				foreach ($transactionList as $transaction) {
+					$overview['transaction_number']++;
+					if ($transaction['status'] == Oara_Utilities::STATUS_CONFIRMED) {
+						$overview['transaction_confirmed_value'] += $transaction['amount'];
+						$overview['transaction_confirmed_commission'] += $transaction['commission'];
+					} else
+					if ($transaction['status'] == Oara_Utilities::STATUS_PENDING) {
+						$overview['transaction_pending_value'] += $transaction['amount'];
+						$overview['transaction_pending_commission'] += $transaction['commission'];
+					} else
+					if ($transaction['status'] == Oara_Utilities::STATUS_DECLINED) {
+						$overview['transaction_declined_value'] += $transaction['amount'];
+						$overview['transaction_declined_commission'] += $transaction['commission'];
+					} else
+					if ($transaction['status'] == Oara_Utilities::STATUS_PAID) {
+						$overview['transaction_paid_value'] += $transaction['amount'];
+						$overview['transaction_paid_commission'] += $transaction['commission'];
+					}
+				}
+				$overviewArray[] = $overview;
+			}
+		}
+
+		return $overviewArray;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * 
+	 * @see Oara/Network/Oara_Network_Publisher_Base#getPaymentHistory()
+	 */
+	public function getPaymentHistory() {
+		$paymentHistory = array ();
+		
+		$urls = array ();
+		$urls [] = new Oara_Curl_Request ( 'https://publisher.ebaypartnernetwork.com/PublisherAccountPaymentHistory', array () );
+		$exportReport = $this->_client->get ( $urls );
+		
+		$dom = new Zend_Dom_Query ( $exportReport [0] );
+		$results = $dom->query ( 'table .aruba_report_table' );
+		if (count ( $results ) > 0) {
+			$exportData = self::htmlToCsv ( self::DOMinnerHTML ( $results->current () ) );
+			for($j = 1; $j < count ( $exportData ); $j ++) {
+				
+				$paymentExportArray = str_getcsv ( $exportData [$j], ";" );
+				$obj = array ();
+				$paymentDate = new Zend_Date ( $paymentExportArray [0], "dd/MM/yy", "en" );
+				$obj ['date'] = $paymentDate->toString ( "yyyy-MM-dd HH:mm:ss" );
+				$obj ['pid'] = $paymentDate->toString ( "yyyyMMdd" );
+				$obj ['method'] = 'BACS';
+				if (preg_match ( "/[-+]?[0-9]*,?[0-9]*\.?[0-9]+/", $paymentExportArray [2], $matches )) {
+					$obj ['value'] = Oara_Utilities::parseDouble ( $matches [0] );
+				} else {
+					throw new Exception ( "Problem reading payments" );
+				}
+				
+				$paymentHistory [] = $obj;
+			}
+		}
+		
+		return $paymentHistory;
+	}
+	
+	/**
+	 *
+	 *
+	 * Function that Convert from a table to Csv
+	 * 
+	 * @param unknown_type $html        	
+	 */
+	private function htmlToCsv($html) {
+		$html = str_replace ( array (
+				"\t",
+				"\r",
+				"\n" 
+		), "", $html );
+		$csv = "";
+		$dom = new Zend_Dom_Query ( $html );
+		$results = $dom->query ( 'tr' );
+		$count = count ( $results ); // get number of matches: 4
+		foreach ( $results as $result ) {
+			$tdList = $result->childNodes;
+			$tdNumber = $tdList->length;
+			for($i = 0; $i < $tdNumber; $i ++) {
+				$value = $tdList->item ( $i )->nodeValue;
+				if ($i != $tdNumber - 1) {
+					$csv .= trim ( $value ) . ";";
+				} else {
+					$csv .= trim ( $value );
+				}
+			}
+			$csv .= "\n";
+		}
+		$exportData = str_getcsv ( $csv, "\n" );
+		return $exportData;
+	}
+	/**
+	 *
+	 *
+	 * Function that returns the innet HTML code
+	 * 
+	 * @param unknown_type $element        	
+	 */
+	private function DOMinnerHTML($element) {
+		$innerHTML = "";
+		$children = $element->childNodes;
+		foreach ( $children as $child ) {
+			$tmp_dom = new DOMDocument ();
+			$tmp_dom->appendChild ( $tmp_dom->importNode ( $child, true ) );
+			$innerHTML .= trim ( $tmp_dom->saveHTML () );
+		}
+		return $innerHTML;
+	}
+}
