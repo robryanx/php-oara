8f9361ae4ab5701391187fc6dc69259a121d07b6
Carlos Morillo <carlos@affjet.com>
2014-03-19 10:53:44 +0000

    new file:   Oara/Network/Publisher/HasOffers.php 	deleted:    Oara/Network/Publisher/TenThousandHours.php

---
+++ Oara/Network/Publisher/HasOffers.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Api Class
+ *
+ * @author Carlos Morillo Merino
+ * @category Oara_Network_Publisher_HasOffers
+ * @copyright Fubra Limited
+ * @version Release: 01.00
+ *         
+ *         
+ */
+class Oara_Network_Publisher_HasOffers extends Oara_Network {
+	/**
+	 * Client
+	 */
+	private $_domain = null;
+	/**
+	 * Password
+	 */
+	private $_apiPassword = null;
+	
+	/**
+	 * Constructor.
+	 *
+	 * @param
+	 *        	$affiliateWindow
+	 * @return Oara_Network_Publisher_Aw_Api
+	 */
+	public function __construct($credentials) {
+		ini_set ( 'default_socket_timeout', '120' );
+		
+		$this->_domain = $credentials["domain"];
+		$this->_apiPassword = $credentials["apiPassword"];
+		
+	}
+	/**
+	 * Check the connection
+	 */
+	public function checkConnection() {
+		$connection = false;
+		
+		$apiURL = "http://api.hasoffers.com/v3/Affiliate_Offer.json?Method=findMyOffers&api_key={$this->_apiPassword}&NetworkId={$this->_domain}";
+		$response = self::call($apiURL);
+		if (count($response["response"]["errors"]) == 0){
+			$connection = true;
+		}
+		return $connection;
+	}
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see library/Oara/Network/Oara_Network_Publisher_Base#getMerchantList()
+	 */
+	public function getMerchantList() {
+		$merchants = array ();
+		$apiURL = "http://api.hasoffers.com/v3/Affiliate_Offer.json?Method=findMyOffers&api_key={$this->_apiPassword}&NetworkId={$this->_domain}";
+		$response = self::call($apiURL);
+		
+		foreach ($response["response"]["data"] as $merchant){
+			
+			$obj = Array ();
+			$obj ['cid'] = $merchant["Offer"]["id"];
+			$obj ['name'] = $merchant["Offer"]["name"];
+			$merchants [] = $obj;
+		}
+		
+		return $merchants;
+	}
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see library/Oara/Network/Oara_Network_Publisher_Base#getTransactionList($merchantId,$dStartDate,$dEndDate)
+	 */
+	public function getTransactionList($merchantList = null, Zend_Date $dStartDate = null, Zend_Date $dEndDate = null, $merchantMap = null) {
+		$totalTransactions = array ();
+		
+		//fields[]=Stat.offer_id&fields[]=Stat.datetime&fields[]=Offer.name&fields[]=Stat.conversion_status&fields[]=Stat.payout&fields[]=Stat.conversion_sale_amount&fields[]=Stat.ip&fields[]=Stat.ad_id&fields[]=Stat.affiliate_info1&sort[Stat.datetime]=desc&filters[Stat.date][conditional]=BETWEEN&filters[Stat.date][values][]=2014-03-13&filters[Stat.date][values][]=2014-03-19&data_start=2014-03-13&data_end=2014-03-19
+		
+		
+		
+		$apiURL = "http://api.hasoffers.com/v3/Affiliate_Report.json?Method=getConversions&api_key={$this->_apiPassword}&NetworkId={$this->_domain}&fields[]=Stat.offer_id&fields[]=Stat.datetime&fields[]=Offer.name&fields[]=Stat.conversion_status&fields[]=Stat.payout&fields[]=Stat.conversion_sale_amount&fields[]=Stat.ip&fields[]=Stat.ad_id&fields[]=Stat.affiliate_info1&sort[Stat.datetime]=desc&filters[Stat.date][conditional]=BETWEEN&filters[Stat.date][values][]={$dStartDate->toString("yyyy-MM-dd")}&filters[Stat.date][values][]={$dEndDate->toString("yyyy-MM-dd")}&data_start={$dStartDate->toString("yyyy-MM-dd")}&data_end={$dEndDate->toString("yyyy-MM-dd")}";
+		$response = self::call($apiURL);
+		foreach ($response["response"]["data"]["data"] as $transactionApi){
+				
+			$transaction = Array();
+			$merchantId = (int) $transactionApi["Stat"]["offer_id"];
+			
+			if (in_array($merchantId, $merchantList)){
+				$transaction['merchantId'] = $merchantId;
+					
+				$transactionDate = new Zend_Date($transactionApi["Stat"]["datetime"], 'yyyy-MM-dd HH:mm:ss', 'en');
+				$transaction['date'] = $transactionDate->toString("yyyy-MM-dd HH:mm:ss");
+					
+				if ($transactionApi["Stat"]["ad_id"] != null) {
+					$transaction['custom_id'] = $transactionApi["Stat"]["ad_id"];
+				}
+					
+				if ($transactionApi["Stat"]["conversion_status"] == "approved") {
+					$transaction['status'] = Oara_Utilities::STATUS_CONFIRMED;
+				} else
+				if ($transactionApi["Stat"]["conversion_status"] == "pending") {
+					$transaction['status'] = Oara_Utilities::STATUS_PENDING;
+				} else
+				if ($transactionApi["Stat"]["conversion_status"] == "rejected") {
+					$transaction['status'] = Oara_Utilities::STATUS_DECLINED;
+				}
+					
+				$transaction['amount'] = $transactionApi["Stat"]["conversion_sale_amount"];
+				$transaction['commission'] = $transactionApi["Stat"]["payout"];
+				$totalTransactions[] = $transaction;
+				
+			}
+			
+			
+				
+		}
+		return $totalTransactions;
+	}
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see library/Oara/Network/Oara_Network_Publisher_Base#getOverviewList($merchantId,$dStartDate,$dEndDate)
+	 */
+	public function getOverviewList($transactionList = null, $merchantList = null, Zend_Date $dStartDate = null, Zend_Date $dEndDate = null, $merchantMap = null) {
+		$overviewArray = Array ();
+		$transactionArray = Oara_Utilities::transactionMapPerDay ( $transactionList );
+		
+		// Add transactions
+		foreach ( $transactionArray as $merchantId => $merchantTransaction ) {
+			foreach ( $merchantTransaction as $date => $transactionList ) {
+				
+				$overview = Array ();
+				
+				$overview ['merchantId'] = $merchantId;
+				$overviewDate = new Zend_Date ( $date, "yyyy-MM-dd" );
+				$overview ['date'] = $overviewDate->toString ( "yyyy-MM-dd HH:mm:ss" );
+				unset ( $overviewDate );
+				$overview ['click_number'] = 0;
+				$overview ['impression_number'] = 0;
+				$overview ['transaction_number'] = 0;
+				$overview ['transaction_confirmed_value'] = 0;
+				$overview ['transaction_confirmed_commission'] = 0;
+				$overview ['transaction_pending_value'] = 0;
+				$overview ['transaction_pending_commission'] = 0;
+				$overview ['transaction_declined_value'] = 0;
+				$overview ['transaction_declined_commission'] = 0;
+				$overview ['transaction_paid_value'] = 0;
+				$overview ['transaction_paid_commission'] = 0;
+				foreach ( $transactionList as $transaction ) {
+					$overview ['transaction_number'] ++;
+					if ($transaction ['status'] == Oara_Utilities::STATUS_CONFIRMED) {
+						$overview ['transaction_confirmed_value'] += $transaction ['amount'];
+						$overview ['transaction_confirmed_commission'] += $transaction ['commission'];
+					} else if ($transaction ['status'] == Oara_Utilities::STATUS_PENDING) {
+						$overview ['transaction_pending_value'] += $transaction ['amount'];
+						$overview ['transaction_pending_commission'] += $transaction ['commission'];
+					} else if ($transaction ['status'] == Oara_Utilities::STATUS_DECLINED) {
+						$overview ['transaction_declined_value'] += $transaction ['amount'];
+						$overview ['transaction_declined_commission'] += $transaction ['commission'];
+					} else if ($transaction ['status'] == Oara_Utilities::STATUS_PAID) {
+						$overview ['transaction_paid_value'] += $transaction ['amount'];
+						$overview ['transaction_paid_commission'] += $transaction ['commission'];
+					}
+				}
+				$overviewArray [] = $overview;
+			}
+		}
+		
+		return $overviewArray;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see Oara/Network/Oara_Network_Publisher_Base#getPaymentHistory()
+	 */
+	public function getPaymentHistory() {
+		$paymentHistory = array ();
+		
+		return $paymentHistory;
+	}
+	
+	private function call($apiUrl){
+		
+		
+		// Initiate the REST call via curl
+		$ch = curl_init($apiUrl);
+			
+		// Set the HTTP method to GET
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+		// Don't return headers
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		// Return data after call is made
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		// Execute the REST call
+		$response = curl_exec($ch);
+		
+		$array = json_decode($response, true);
+		// Close the connection
+		curl_close($ch);
+		return $array;
+	}
+}
