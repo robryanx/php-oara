b5060abea4a45d36f74eb9b530d49ec597c3261f
Carlos Morillo <carlos@affjet.com>
2014-05-30 08:59:46 +0100

    modified:   Oara/Network/Publisher/Effiliation.php

--- Oara/Network/Publisher/Effiliation.php
+++ Oara/Network/Publisher/Effiliation.php
@@ -30,7 +30,7 @@ class Oara_Network_Publisher_Effiliation extends Oara_Network {
 	public function checkConnection() {
 		$connection = false;
 
-		$content = file_get_contents('http://api.effiliation.com/api/transaction.csv?key='.$this->_credentials["apiPassword"]);
+		$content = file_get_contents('http://api.effiliation.com/apiv2/transaction.csv?key='.$this->_credentials["apiPassword"]);
 		if (!preg_match("/bad credentials !/", $content, $matches)) {
 			$connection = true;
 		}
@@ -43,17 +43,15 @@ class Oara_Network_Publisher_Effiliation extends Oara_Network {
 	 */
 	public function getMerchantList() {
 		$merchants = array();
-		
-		$content = @file_get_contents('http://api.effiliation.com/api/programmes.xml?key='.$this->_credentials["apiPassword"]);
+		$url = 'http://api.effiliation.com/apiv2/programs.xml?key='.$this->_credentials["apiPassword"]."&filter=active";
+		$content = @file_get_contents($url);
 		$xml = simplexml_load_string($content, null, LIBXML_NOERROR | LIBXML_NOWARNING);
-		foreach ($xml->programme as $merchant) {
-			if ((string) $merchant->etat == "inscrit") {
-				$obj = array();
-				$obj['cid'] = (string) $merchant->id_programme;
-				$obj['name'] = (string) $merchant->nom;
-				$obj['url'] = "";
-				$merchants[] = $obj;
-			}
+		foreach ($xml->program as $merchant) {
+			$obj = array();
+			$obj['cid'] = (string) $merchant->id_programme;
+			$obj['name'] = (string) $merchant->nom;
+			$obj['url'] = "";
+			$merchants[] = $obj;
 		}
 		return $merchants;
 	}
@@ -65,7 +63,8 @@ class Oara_Network_Publisher_Effiliation extends Oara_Network {
 	public function getTransactionList($merchantList = null, Zend_Date $dStartDate = null, Zend_Date $dEndDate = null, $merchantMap = null) {
 		$totalTransactions = array();
 
-		$content = file_get_contents('http://api.effiliation.com/api/transaction.csv?key='.$this->_credentials["apiPassword"].'&start='.$dStartDate->toString("dd/MM/yyyy").'&end='.$dEndDate->toString("dd/MM/yyyy").'&type=datetran');
+		$url = 'http://api.effiliation.com/apiv2/transaction.csv?key='.$this->_credentials["apiPassword"].'&start='.$dStartDate->toString("dd/MM/yyyy").'&end='.$dEndDate->toString("dd/MM/yyyy").'&type=datetran';
+		$content = file_get_contents($url);
 		$exportData = str_getcsv($content, "\n");
 		$num = count($exportData);
 		for ($i = 1; $i < $num; $i++) {
