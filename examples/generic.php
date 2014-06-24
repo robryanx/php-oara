3d7e982e9f019d6cb245eae8a3c9a3509a621c84
Carlos Morillo <carlos@affjet.com>
2014-06-24 12:30:46 +0100

    modified:   examples/generic.php

--- examples/generic.php
+++ examples/generic.php
@@ -52,7 +52,7 @@ require realpath(dirname(__FILE__)).'/../settings.php';
  * CommissionJunction
  * ShareASale
  */
-$networkName = "Ibiboads"; //Ex: AffiliateWindow
+$networkName = "AffiliateWindow"; //Ex: AffiliateWindow
 //Retrieving the credentials for the network
 $config = Zend_Registry::getInstance()->get('credentialsIni');
 
