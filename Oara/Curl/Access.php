5284b159afe537a4049ca066f3e11eedaa38c215
Carlos Morillo <carlos@affjet.com>
2014-09-24 15:04:25 +0100

    new file:   LICENSE.TXT 	modified:   Oara/Curl/Access.php 	modified:   Oara/Curl/Parameter.php 	modified:   Oara/Curl/Request.php

--- Oara/Curl/Access.php
+++ Oara/Curl/Access.php
@@ -1,5 +1,22 @@
 <?php
 /**
+   The goal of the Open Affiliate Report Aggregator (OARA) is to develop a set 
+   of PHP classes that can download affiliate reports from a number of affiliate networks, and store the data in a common format.
+   
+    Copyright (C) 2014  Carlos Morillo Merino
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	and we should add some contact information
+**/	
+	
+/**
 * Acces Class
 *
 * @author Carlos Morillo Merino
