8912d99559307364d713b55e3afef5112e240334
Piotr Karecki <pkarecki@gmail.com>
2017-01-06 09:52:23 +0100

    validation fix, all data if merchantList null
    Same problem here for spaces in empty string. 
    IMO if no merchantList then all data should be returned

--- Oara/Network/Publisher/TradeDoubler.php
+++ Oara/Network/Publisher/TradeDoubler.php
@@ -425,7 +425,7 @@ class TradeDoubler extends \Oara\Network
             if (\count($this->_sitesAllowed) == 0 || \in_array($transactionExportArray[13], $this->_sitesAllowed)) {
 
 
-                if ($transactionExportArray[0] !== '' && isset($merchantIdList[(int)$transactionExportArray[2]])) {
+                if ($transactionExportArray[0] && (is_null($merchantList) || isset($merchantIdList[(int)$transactionExportArray[2]]))) {
 
                     $transaction = Array();
                     $transaction['merchantId'] = $transactionExportArray[2];
